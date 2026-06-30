f89b80688122bdc85a095c40c583607530f77776
isaacbolili2001-debug <isaacbolili2001@gmail.com>
2026-06-30 11:57:15 +0100

    cool

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        string first_name;
+        string last_name;
+        Console.WriteLine("Please enter your first name:");
+        first_name = Console.ReadLine();
+        Console.WriteLine("Please enter your last name:");
+        last_name = Console.ReadLine();
+        Console.WriteLine($"Hello dear {first_name} {last_name}, welcome to the C# programming world!");
+
     }
 }
