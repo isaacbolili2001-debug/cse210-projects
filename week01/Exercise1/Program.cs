37b9d6bb78d93b7e6953b129008e689d38a0f220
isaacbolili2001-debug <isaacbolili2001@gmail.com>
2026-06-30 12:49:35 +0100

    cool

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,13 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-        string first_name;
-        string last_name;
+        string firstName;
+        string lastName;
         Console.WriteLine("Please enter your first name:");
-        first_name = Console.ReadLine();
+        firstName = Console.ReadLine();
         Console.WriteLine("Please enter your last name:");
-        last_name = Console.ReadLine();
-        Console.WriteLine($"Hello dear {first_name} {last_name}, welcome to the C# programming world!");
+        lastName = Console.ReadLine();
+        Console.WriteLine($"Hello dear {firstName} {lastName}, welcome to the C# programming world!");
 
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your age ?: ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        Console.WriteLine($"Your age is {number}.");
     }
 }
